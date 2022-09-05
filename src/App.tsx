c79791f9ad29b8ffaaa4b4dd57665d36b4f4f732
Hüseyin BATTAL <95706081+huseyinbattal@users.noreply.github.com>
2022-09-05 22:34:58 +0300

    first commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,21 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useState } from 'react';
 import './App.css';
 
 function App() {
+  const [number, setNumber] = useState(0);
+  const [text,setText]=useState<string>("u")
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+       
+        <button onClick={() => {
+          setNumber(number + 1)
+          setText("üssün")
+        }} >Sayi Arttır</button>
+        <br/> 
+        {number}
+        <br />
+        {text}
       </header>
     </div>
   );
