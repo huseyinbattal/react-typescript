1e0069d7d8e260b539547435c180a5cb3a5c2f0d
Hüseyin BATTAL <95706081+huseyinbattal@users.noreply.github.com>
2022-09-06 17:58:51 +0300

    Create ArrayObjects.tsx

---
+++ src/components/ArrayObjects.tsx
@@ -0,0 +1,16 @@
+export interface PropTypes{
+    userArray: [],
+    userObj: {},
+    
+}
+
+
+const ArrayObjects=(props:PropTypes)=> {
+  return (
+      <div>
+  
+      </div>
+  )
+}
+
+export default ArrayObjects
